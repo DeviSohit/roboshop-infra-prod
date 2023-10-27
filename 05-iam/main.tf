0c8e877e0b2413e66026ba5a9901fdcc60a6bba3
devi <devimeena.iiit@gmail.com>
2023-10-27 23:29:34 +0530

    added libraries

---
+++ 05-iam/main.tf
@@ -0,0 +1,29 @@
+# creating a IAM role to catalogue EC2 instance
+resource "aws_iam_role" "catalogue" {
+  name = "CatalogueEC2-${var.env}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  # attaching policy or permission of AmazonSSMReadOnlyAccess to Catalogue-prod Role
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"]
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_instance_profile" "catalogue_profile" {
+  name = "catalogue-profile-${var.env}"
+  role = aws_iam_role.catalogue.name
+}
